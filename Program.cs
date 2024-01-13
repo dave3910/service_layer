3c8ab4060168f31ba001219057750a6a67d11f98
David Mechato <jmechato@vsperu.com>
2024-01-13 09:07:09 -0500

    Proyecto base service layer

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service_layer
+{
+    internal class Program
+    {
+        private static SLManager sLManager;
+        static  void Main(string[] args)
+        {
+            
+
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.SystemDefault;
+
+            try
+            {
+                ConectarSL();
+                CrearSocioDeNegocio();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR : {ex.Message}");
+            }
+            finally
+            { 
+                Console.ReadKey();
+            }
+        }
+
+        private static void CrearSocioDeNegocio()
+        {
+            try
+            {
+                var socioNegocio = new { CardCode = "CL70393849", CardName = "SEMILLERO 2023", FederalTaxID = "70393849" };
+
+                string responseSocio = sLManager.Post("BusinessPartners", socioNegocio);
+                Console.WriteLine($"SOCIO DE NEGOCIO CREADO EXITOSAMENTE!");
+                Console.WriteLine(responseSocio);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[CREACIÓN DE SOCIO] - {ex.Message}");
+            }
+        }
+
+        private static void ConectarSL()
+        {
+            try
+            {
+                string serviceLayerUrl = "https://192.168.1.14:50000/b1s/v1";
+                LoginData loginData = new LoginData()
+                {
+                    CompanyDB = "02_LARAMA_PRODUCCION",
+                    UserName = "DESARROLLO02",
+                    Password = "Pisco.2030"
+                };
+
+                sLManager = new SLManager(serviceLayerUrl, loginData);
+                Console.WriteLine($"[CONEXIÓN SL] - SESSION ID: {sLManager.GetSessionId()}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[CONEXIÓN SL] - {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SLManager.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service_layer
+{
+    public class SLManager
+    {
+        private string SLRoot;
+        private LoginData loginData;
+        private readonly RestClient restClient;
+        private string sessionID;
+        public SLManager(string root, LoginData loginData)
+        {
+            this.SLRoot = root;
+            this.loginData = loginData;
+            restClient = new RestClient(this.SLRoot);
+            Login();
+        }
+
+        public string GetSessionId()
+        {
+            return sessionID;
+        }
+        private void Login()
+        {
+            try
+            {
+                var request = new RestRequest("Login", Method.POST);
+
+                request.AddHeader("Accept", "application/json");
+                request.AddJsonBody(loginData);
+
+                var response = restClient.Execute(request);
+
+                if (response.IsSuccessful)
+                {
+                    LoginResponse loginResponse = JsonConvert.DeserializeObject<LoginResponse>(response.Content);
+                    sessionID = loginResponse.SessionId;
+                    restClient.AddDefaultHeader("Authorization", $"Bearer {loginResponse.SessionId}");
+                }
+                else
+                {
+                    SLError error = JsonConvert.DeserializeObject<SLError>(response.Content);
+                    throw new Exception(error.error.message.value);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public string Post(string resource, object data)
+        {
+            try
+            {
+                var request = new RestRequest($"{resource}", Method.POST);
+                request.AddJsonBody(data);
+
+                var response = restClient.Execute(request);
+
+                if (!response.IsSuccessful)
+                {
+                    SLError error = JsonConvert.DeserializeObject<SLError>(response.Content);
+                    throw new Exception(error.error.message.value);
+                }
+
+                return response.Content;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class LoginData
+    {
+        public string CompanyDB { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string SessionId { get; set; }
+        public string Version { get; set; }
+        public int SessionTimeOut { get; set; }
+    }
+    public class SLError
+    {
+        public Error error { get; set; }
+    }
+
+    public class Error 
+    {
+        public int code { get; set; }
+        public ErrorMessage message { get; set; }
+    }
+
+    public class ErrorMessage
+    {
+        public string lang { get; set; }
+        public string value { get; set; }
+    }
+}
